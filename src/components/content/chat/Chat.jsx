26457a893f7caab6fe56795ac5a2b8408759ae53
Danilo <didoherculano@gmail.com>
2021-11-14 14:56:48 -0300

    layout básico adicionado

---
+++ src/components/content/chat/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './chat.scss'
+
+function Chat() {
+    return ( 
+        <div className="chat">
+            CHat
+        </div>
+     );
+}
+
+export default Chat;
--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './app.scss';
 
+import Navigator from '../navigator/Navigator'
+import Header from '../content/header/Header'
+import Chat from '../content/chat/Chat'
+
 function App() {
   return (
     <div className="App">
-      App
+      <Navigator/>
+      <div className="content">
+        <Header/>
+        <Chat/>
+      </div>
     </div>
   );
 }
